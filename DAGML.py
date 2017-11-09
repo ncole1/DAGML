9d4943b0a99add567130c70e28b6bd280f349510
Justin Terry <justinkterry@gmail.com>
2017-11-09 01:40:06 -0500

    open close

--- DAGML.py
+++ DAGML.py
@@ -1,1 +1,8 @@
-
+class Rake(object):
+  def __init__(self, path):
+    file=open(path,'r+')
+    self.DAG=file.read().split('\n)
+    file.close()
+  def save(path):
+    file=open(path,'w+')
+    file.writelines(self.DAG)
