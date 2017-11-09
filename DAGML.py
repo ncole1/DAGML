c10931aea6b06d343f3d2e506ff74869991b0940
Justin Terry <justinkterry@gmail.com>
2017-11-09 15:44:09 -0500

    branchesInDAG and misc small fixes

--- DAGML.py
+++ DAGML.py
@@ -1,11 +1,12 @@
 class Rake(object):
   def __init__(self, path):
     file=open(path,'r+')
-    self.DAG=file.read().split('\n)
+    self.DAG=file.read().split('\n')
     file.close()
   def save(path):
     file=open(path,'w+')
     file.writelines(self.DAG)
+    file.close()
   def addOperator(branchNumber,operator):
     self.DAG.find('<'+branchNumber+
                                
@@ -15,7 +16,11 @@ class Rake(object):
   def addMerge
   def removeMerge
   def branchesInDAG: #return name list
-    return [i for i, x in enumerate(my_list) if x == "<"]
+    output=[]
+    for i in range(0,self.DAG.__len__):
+      if self.DAG[i][0]=='<':
+      output.append(self.DAG[i][1])
+    return output
   def operatorsInBranch
   def editCut
   
