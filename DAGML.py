34c702f558481a82a2e8b15d839b0c6c9d037d12
Justin Terry <justinkterry@gmail.com>
2017-11-09 19:35:35 -0500

    Update DAGML.py

--- DAGML.py
+++ DAGML.py
@@ -1,29 +1,40 @@
 class Rake(object):
+  
   def __init__(self, path):
     file=open(path,'r+')
     self.DAG=file.read().split('\n')
     file.close()
+    
   def save(path):
     file=open(path,'w+')
     file.writelines(self.DAG)
     file.close()
+    
   def addOperator(branchNumber,operator): #in progress
     for i in range(0,self.DAG.__len__):
       if self.DAG[i][0].strip()=='<'+str(branch):
         branchStart=i
         break
+        
   def editCut
+  
   def removeOperator
+  
   def addBranch
+  
   def removeBranch
+  
   def addMerge
+  
   def removeMerge
+  
   def branchesInDAG: #return name list
     branches=[]
     for i in range(0,self.DAG.__len__):
       if self.DAG[i][0].strip()=='<':
         branches.append(self.DAG[i][1])
     return branches
+  
   def operatorsInBranch(branch):
     for i in range(0,self.DAG.__len__):
       if self.DAG[i][0].strip()=='<'+str(branch):
@@ -34,5 +45,3 @@ class Rake(object):
         branchEnd=i
         break
     return branchEnd-branchStart-1
-
-  
